2b14caa04586eb8fa17d0096f1ce1cb088814e50
emPeeGee <eempeegee@gmail.com>
2023-12-30 22:17:17 +0200

    🚧 feat: [wip] init the work on custom font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
+import { Text } from 'react-native';
+
 import { ThemeProvider } from '@/ui/theme';
 import { ApplicationConfigurator } from './ApplicationConfigurator';
+import { useFonts, Fredoka_500Medium } from '@expo-google-fonts/fredoka';
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Fredoka_500Medium,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
+      <Text style={{ fontFamily: 'Fredoka_500Medium', fontSize: 40 }}>Inter Black</Text>
       <ApplicationConfigurator />
     </ThemeProvider>
   );
