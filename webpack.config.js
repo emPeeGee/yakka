96d3fc8bb52dba64c6a260325ed01f17a8a9ff7e
Mihail Popovici <end63347@adobe.com>
2024-01-31 11:17:43 +0200

    ✨ feat: add web support in the app

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  config.resolve.alias['@/ui'] = path.resolve('./src/ui');
+  config.resolve.alias['@/core'] = path.resolve('./src/core');
+  config.resolve.alias['@/navigation'] = path.resolve('./src/navigation');
+  config.resolve.alias['@/types'] = path.resolve('./src/types');
+  config.resolve.alias['@/screens'] = path.resolve('./src/screens');
+
+  return config;
+};
