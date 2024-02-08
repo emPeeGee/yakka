b6ceee93cdb00c21adae52989d78a77e9191f0bf
emPeeGee <eempeegee@gmail.com>
2024-02-08 20:34:28 +0200

    🐞 fix: spacing issue in the OnboardgetStartedScreen

--- src/screens/onboarding/OnboardGetStartedScreen.tsx
+++ src/screens/onboarding/OnboardGetStartedScreen.tsx
@@ -23,38 +23,42 @@ export const OnboardGetStartedScreen = () => {
           paddingRight: insets.right,
         },
       ]}>
-      <View style={[gStyles.fullWidthFromStart, { justifyContent: 'space-evenly' }]}>
-        {/* TODO: tx */}
-        <HeroWithChat text="Hi there. I'm Yakka!" chatPosition="top" hero="vampire" withConfetti />
+      <View style={{ flex: 1, paddingHorizontal: theme.spacing.md }}>
+        <View style={[gStyles.fullWidthFromStart, { justifyContent: 'space-evenly' }]}>
+          <HeroWithChat tx="onboard.greeting1" chatPosition="top" hero="vampire" withConfetti />
 
-        <EnhancedText
-          size="xxxl"
-          weight="medium"
-          style={{ color: theme.colors.primary700, textAlign: 'center' }}>
-          Yakka
-        </EnhancedText>
-        <View>
-          <EnhancedText size="sm" weight="medium" style={{ textAlign: 'center' }}>
-            Learn English language whenever and wherever you want.
-          </EnhancedText>
+          <EnhancedText
+            tx="universal.yakka"
+            size="xxxl"
+            weight="medium"
+            style={{ color: theme.colors.primary700, textAlign: 'center' }}
+          />
+          <View>
+            <EnhancedText
+              tx="onboard.benefit1"
+              size="sm"
+              weight="medium"
+              style={{ textAlign: 'center' }}
+            />
+          </View>
         </View>
-      </View>
 
-      <View
-        style={[
-          { width: '100%', gap: theme.spacing.md, paddingVertical: theme.spacing.lg },
-          gStyles.centerColumn,
-        ]}>
-        <Button
-          tx="onboard.getStarted"
-          color={theme.colors.base0}
-          onPress={() => navigate('OnboardQuestions' as never)}
-        />
-        <Button
-          tx="onboard.haveAnAccount"
-          backgroundColor={theme.colors.primary100}
-          color={theme.colors.primary900}
-        />
+        <View
+          style={[
+            { width: '100%', gap: theme.spacing.md, paddingVertical: theme.spacing.md },
+            gStyles.centerColumn,
+          ]}>
+          <Button
+            tx="onboard.getStarted"
+            color={theme.colors.base0}
+            onPress={() => navigate('OnboardQuestions' as never)}
+          />
+          <Button
+            tx="onboard.haveAnAccount"
+            backgroundColor={theme.colors.primary100}
+            color={theme.colors.primary900}
+          />
+        </View>
       </View>
     </View>
   );
