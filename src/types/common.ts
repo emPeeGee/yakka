02f1df140d8875ae198772366a573207307471e0
emPeeGee <eempeegee@gmail.com>
2024-02-23 18:02:22 +0200

    🌈 style: jsdoc the spacing values

--- src/types/common.ts
+++ src/types/common.ts
@@ -6,13 +6,22 @@ export type SelectableOption<T> = {
 };
 
 export type Dimensions = {
+  /** 2 */
   xxxs: number;
+  /**  4 */
   xxs: number;
+  /** 8 */
   xs: number;
+  /** 12 */
   sm: number;
+  /** 16 */
   md: number;
+  /** 24 */
   lg: number;
+  /** 32 */
   xl: number;
+  /** 48 */
   xxl: number;
+  /** 64 */
   xxxl: number;
 };
