6fa7712993f6c44cdaea554eefc5ab4e379a05e0
emPeeGee <eempeegee@gmail.com>
2024-02-24 19:55:18 +0200

    🐞 fix: word of the day background

--- src/screens/vocabulary/VocWordOfTheDayScreen.tsx
+++ src/screens/vocabulary/VocWordOfTheDayScreen.tsx
@@ -8,22 +8,15 @@ import vocabulary from '../../mocks/vocabulary.json';
 
 // TODO: word of the day will be provided via network
 export const VocWordOfTheDayScreen = () => {
-  const { theme } = useTheme();
+  const { theme, appColorScheme } = useTheme();
   const gStyles = useGlobalThemedStyles();
 
   return (
-    <ContainerWithInsets>
-      <View
-        style={[
-          {
-            flex: 1,
-            width: '100%',
-            gap: theme.spacing.md,
-            paddingVertical: theme.spacing.md,
-            backgroundColor: theme.colors.primary100,
-          },
-          gStyles.centerColumn,
-        ]}>
+    <ContainerWithInsets
+      backgroundColor={
+        appColorScheme === 'light' ? theme.colors.primary100 : theme.colors.background
+      }>
+      <View style={[gStyles.centerColumn]}>
         <View style={{ minWidth: 320, minHeight: 560 }}>
           <WordCard word={vocabulary.mighty as Word} />
         </View>
