30daeb70e7ea49ce6b544f39ac26162a6faebf11
emPeeGee <eempeegee@gmail.com>
2024-03-13 18:15:26 +0200

    🌈 style: remove unused old code

--- src/screens/learn/LearnLessonScreen.tsx
+++ src/screens/learn/LearnLessonScreen.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { ReactElement, useEffect, useMemo, useRef, useState } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { View } from 'react-native';
 
 import { StackActions, useNavigation } from '@react-navigation/native';
 import ActionSheet, { ActionSheetRef } from 'react-native-actions-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useSharedValue, runOnUI, runOnJS } from 'react-native-reanimated';
 
 import { LESSON_DONE_DATA_KEY } from '@/core/constants';
 import { rootLog } from '@/core/logger';
@@ -26,101 +25,9 @@ import {
 } from '@/ui/core';
 import { useTheme } from '@/ui/theme';
 import { DragWordsActivity } from './DragWordsActivity';
-import { MARGIN_LEFT } from './Layout';
-import { Lines } from './Lines';
 import { PickAnswerActivity } from './PickAnswer';
-import { SortableWord } from './SortableWord';
 import { TypeAnswerActivity } from './TypeAnswer';
 import { parseRawWizardDataQuestion } from './utils/parseRawWizardDataQuestions';
-// import { Word } from './Word';
-
-// const words = [
-//   { id: 1, word: 'Er' },
-//   { id: 8, word: 'hungrig' },
-//   { id: 2, word: 'isst' },
-//   { id: 7, word: 'er' },
-//   { id: 6, word: 'weil' },
-//   { id: 9, word: 'ist' },
-//   { id: 5, word: ',' },
-//   { id: 3, word: 'einen' },
-//   { id: 4, word: 'Apfel' },
-// ];
-
-const containerWidth = Dimensions.get('window').width - MARGIN_LEFT * 2;
-const wordListStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: MARGIN_LEFT,
-  },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    opacity: 0,
-  },
-});
-
-interface WordListProps {
-  children: ReactElement<{ id: number }>[];
-}
-
-export const WordList = ({ children }: WordListProps) => {
-  const [ready, setReady] = useState(false);
-  const offsets = children.map(() => ({
-    order: useSharedValue(0),
-    width: useSharedValue(0),
-    height: useSharedValue(0),
-    x: useSharedValue(0),
-    y: useSharedValue(0),
-    originalX: useSharedValue(0),
-    originalY: useSharedValue(0),
-  }));
-  if (!ready) {
-    return (
-      <View style={wordListStyles.row}>
-        {children.map((child, index) => {
-          return (
-            <View
-              // eslint-disable-next-line react/no-array-index-key
-              key={index}
-              onLayout={({
-                nativeEvent: {
-                  layout: { x, y, width, height },
-                },
-              }) => {
-                const offset = offsets[index]!;
-                offset.order.value = -1;
-                offset.width.value = width;
-                offset.height.value = height;
-                offset.originalX.value = x;
-                offset.originalY.value = y;
-                runOnUI(() => {
-                  'worklet';
-                  if (offsets.filter(o => o.order.value !== -1).length === 0) {
-                    runOnJS(setReady)(true);
-                  }
-                })();
-              }}>
-              {child}
-            </View>
-          );
-        })}
-      </View>
-    );
-  }
-  return (
-    <View style={wordListStyles.container}>
-      <Lines />
-      {children.map((child, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <SortableWord key={index} offsets={offsets} index={index} containerWidth={containerWidth}>
-          {child}
-        </SortableWord>
-      ))}
-    </View>
-  );
-};
 
 const lessonActivities: LessonActivity[] = [
   {
