5a1356e86a8df4f7d59116f2abbb307e3e655d4a
emPeeGee <eempeegee@gmail.com>
2024-03-15 11:15:52 +0200

    🌱 feat: info box support info tooltip

--- src/ui/core/InfoBox.tsx
+++ src/ui/core/InfoBox.tsx
@@ -2,16 +2,19 @@ import { View } from 'react-native';
 
 import { TxKeyPath } from '@/core/i18n';
 import { EnhancedText } from './EnhancedText';
+import { InfoIcon } from '../icons';
 import { useTheme, useGlobalThemedStyles } from '../theme';
+import Tooltip from '../Tooltip/Tooltip';
 
 type InfoBoxProps = {
   color: string;
   txTitle: TxKeyPath;
   value: string | number;
   Icon: () => JSX.Element;
+  txInfo?: TxKeyPath;
 };
 
-export const InfoBox = ({ color, txTitle, value: text, Icon }: InfoBoxProps) => {
+export const InfoBox = ({ color, txTitle, txInfo, value: text, Icon }: InfoBoxProps) => {
   const { theme } = useTheme();
   const gStyles = useGlobalThemedStyles();
 
@@ -28,12 +31,26 @@ export const InfoBox = ({ color, txTitle, value: text, Icon }: InfoBoxProps) => 
         overflow: 'hidden',
       }}>
       <View style={{ backgroundColor: color, paddingVertical: theme.spacing.sm }}>
-        <EnhancedText
-          tx={txTitle}
-          size="lg"
-          weight="medium"
-          style={{ color: theme.colors.base0, textAlign: 'center' }}
-        />
+        <View style={[gStyles.centerRow, { gap: theme.spacing.xxs }]}>
+          <EnhancedText
+            tx={txTitle}
+            size="lg"
+            weight="medium"
+            style={{ color: theme.colors.base0, textAlign: 'center' }}
+          />
+          {txInfo && (
+            <Tooltip
+              backgroundColor={theme.colors.info}
+              pointerColor={theme.colors.info}
+              popover={<EnhancedText style={{ color: theme.colors.base0 }} tx={txInfo} />}
+              overlayColor=""
+              width="90%"
+              height="auto"
+              withOverlay={false}>
+              <InfoIcon />
+            </Tooltip>
+          )}
+        </View>
       </View>
       <View
         style={[
