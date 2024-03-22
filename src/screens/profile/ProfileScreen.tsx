d825a4538290071da7a91484f59eb2e3bdf83b47
Popovici Mihail <mihail.popovici>
2024-03-22 08:47:54 +0200

    build: add react chart library

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { View, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Image, StyleSheet, ScrollView, Dimensions } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import { SheetManager } from 'react-native-actions-sheet';
@@ -27,6 +27,7 @@ import {
   UserCircleIcon,
 } from '@/ui/icons';
 import { useGlobalThemedStyles, useTheme } from '@/ui/theme';
+import { BarChart, LineChart } from 'react-native-chart-kit';
 // import { ToggleButton } from '@/ui/core/ButtonToggleGroup';
 
 export const ProfileScreen = () => {
@@ -104,7 +105,6 @@ export const ProfileScreen = () => {
           <HeroWithChat direction="left" width={74} height={70} hero="ghost" />
           {/* <ToggleButton primaryText="12" secondaryText="333" onPress={} /> */}
         </View>
-
         <Separator paddingVertical={theme.spacing.md} />
 
         {user && (
@@ -183,19 +183,68 @@ export const ProfileScreen = () => {
             </EnhancedText>
           </View>
         </View>
-
-        <Separator isVertical height={24} paddingVertical={0} />
-
-        <View style={gStyles.centerColumnBetween}>
-          <View>
-            <EnhancedText size="md">1</EnhancedText>
-          </View>
-          <View>
-            <EnhancedText style={styles.statLabel} size="xxs">
-              Languages
-            </EnhancedText>
-          </View>
+        <View>
+          <BarChart
+            yAxisSuffix="k"
+            style={{}}
+            data={{
+              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+              datasets: [
+                {
+                  data: [20, 45, 28, 80, 99, 43],
+                },
+              ],
+            }}
+            width={Dimensions.get('window').width} // from react-native
+            height={220}
+            yAxisLabel="$"
+            chartConfig={{
+              backgroundColor: '#e26a00',
+              backgroundGradientFrom: '#fb8c00',
+              backgroundGradientTo: '#ffa726',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                r: '6',
+                strokeWidth: '2',
+                stroke: '#ffa726',
+              },
+            }}
+            verticalLabelRotation={30}
+          />{' '}
+          {/* yAxisLabel="$"
+            yAxisSuffix="k" */}
+          {/* yAxisInterval={1} // optional, defaults to 1 */}
+          {/* chartConfig={{
+              backgroundColor: '#e26a00',
+              backgroundGradientFrom: '#fb8c00',
+              backgroundGradientTo: '#ffa726',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                r: '6',
+                strokeWidth: '2',
+                stroke: '#ffa726',
+              },
+            }} */}
+          {/* bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+          /> */}
         </View>
+        <ButtonToggleGroup options={['Daily', 'Weekly', 'Monthly']} onOptionChange={noop} />
+        <List title="Dashboard" data={DASHBOARD_LIST} />
+        <List title="My account" data={ACCOUNT_LIST} />
       </View>
 
       <ButtonToggleGroup options={['Daily', 'Weekly', 'Monthly']} onOptionChange={noop} />
