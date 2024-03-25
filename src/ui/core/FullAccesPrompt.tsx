43f7400d655695a3e03b24156e86f9e70c5c3841
emPeeGee <eempeegee@gmail.com>
2024-03-25 13:59:20 +0200

    🐞 fix: applicatin crash due to wrong import of button

--- src/ui/core/FullAccesPrompt.tsx
+++ src/ui/core/FullAccesPrompt.tsx
@@ -3,8 +3,10 @@ import { View, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { BlurView } from 'expo-blur';
 
-import { EnhancedText, Card, Button } from '@/ui/core';
 import { useTheme } from '@/ui/theme';
+import { Button } from './Button';
+import { Card } from './Card';
+import { EnhancedText } from './EnhancedText';
 
 export const FullAccessPrompt = () => {
   const { theme, appColorScheme } = useTheme();
@@ -18,7 +20,7 @@ export const FullAccessPrompt = () => {
         experimentalBlurMethod="dimezisBlurView"
         style={{ height: '100%', alignItems: 'center' }}>
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-          <Card style={{ marginHorizontal: theme.spacing.lg }}>
+          <Card style={{ marginHorizontal: theme.spacing.lg }} maxHeight="auto">
             <View style={{ gap: theme.spacing.md }}>
               <EnhancedText preset="subheading" tx="auth.unlockAccessTitle" />
               <EnhancedText style={theme.typography.sizes.md} tx="auth.unlockAccessDetails" />
